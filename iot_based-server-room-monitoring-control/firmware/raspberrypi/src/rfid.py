9490d5b4c6c353847f5cfbda17de2a1bdf0880c4
tingachris <chrispinetinega@gmail.com>
2025-03-16 22:23:00 +0300

    Add RFID reader module for Raspberry Pi with authentication and data handling

---
+++ iot_based-server-room-monitoring-control/firmware/raspberrypi/src/rfid.py
@@ -0,0 +1,316 @@
+#!/usr/bin/env python3
+"""
+rfid.py
+
+This module interfaces with an RFID reader connected to the Raspberry Pi.
+It provides functions to initialize the reader, read and write data to RFID tags,
+and handle authentication and communication with the tags.
+
+Dependencies:
+  - RPi.GPIO library (install with `pip install RPi.GPIO`)
+  - spi library (included in the project)
+"""
+
+import RPi.GPIO as GPIO
+import spi
+import logging
+import time
+
+logging.basicConfig(level=logging.INFO)
+
+AUTHORIZED_CARDS = {
+  (5, 74, 28, 185, 234): {"name": "Card A", "role": "admin"},
+  (83, 164, 247, 164, 164): {"name": "Card B", "role": "IT staff"},
+  (20, 38, 121, 207, 132): {"name": "Card C", "role": "maintenance"}
+}
+
+class MFRC522:
+  NRSTPD = 22
+  MAX_LEN = 16
+
+  PCD_IDLE = 0x00
+  PCD_AUTHENT = 0x0E
+  PCD_RECEIVE = 0x08
+  PCD_TRANSMIT = 0x04
+  PCD_TRANSCEIVE = 0x0C
+  PCD_RESETPHASE = 0x0F
+  PCD_CALCCRC = 0x03
+
+  PICC_REQIDL = 0x26
+  PICC_REQALL = 0x52
+  PICC_ANTICOLL = 0x93
+  PICC_SELECTTAG = 0x93
+  PICC_AUTHENT1A = 0x60
+  PICC_AUTHENT1B = 0x61
+  PICC_READ = 0x30
+  PICC_WRITE = 0xA0
+  PICC_DECREMENT = 0xC0
+  PICC_INCREMENT = 0xC1
+  PICC_RESTORE = 0xC2
+  PICC_TRANSFER = 0xB0
+  PICC_HALT = 0x50
+
+  MI_OK = 0
+  MI_NOTAGERR = 1
+  MI_ERR = 2
+
+  def __init__(self, spd=1000000):
+    """
+    Initialize the MFRC522 RFID reader.
+    """
+    try:
+      spi.openSPI(speed=spd)
+      GPIO.setmode(GPIO.BOARD)
+      GPIO.setup(self.NRSTPD, GPIO.OUT)
+      GPIO.output(self.NRSTPD, 1)
+      self.MFRC522_Init()
+    except Exception as e:
+      logging.error("Failed to initialize MFRC522: %s", e)
+      self.GPIO_CLEAN()
+
+  def MFRC522_Reset(self):
+    """
+    Reset the MFRC522 RFID reader.
+    """
+    self.Write_MFRC522(self.CommandReg, self.PCD_RESETPHASE)
+
+  def Write_MFRC522(self, addr, val):
+    """
+    Write a value to a register on the MFRC522.
+    """
+    spi.transfer(((addr << 1) & 0x7E, val))
+
+  def Read_MFRC522(self, addr):
+    """
+    Read a value from a register on the MFRC522.
+    """
+    val = spi.transfer((((addr << 1) & 0x7E) | 0x80, 0))
+    return val[1]
+
+  def SetBitMask(self, reg, mask):
+    """
+    Set bits in a register on the MFRC522.
+    """
+    tmp = self.Read_MFRC522(reg)
+    self.Write_MFRC522(reg, tmp | mask)
+
+  def ClearBitMask(self, reg, mask):
+    """
+    Clear bits in a register on the MFRC522.
+    """
+    tmp = self.Read_MFRC522(reg)
+    self.Write_MFRC522(reg, tmp & (~mask))
+
+  def AntennaOn(self):
+    """
+    Turn the antenna on.
+    """
+    temp = self.Read_MFRC522(self.TxControlReg)
+    if ~(temp & 0x03):
+      self.SetBitMask(self.TxControlReg, 0x03)
+
+  def AntennaOff(self):
+    """
+    Turn the antenna off.
+    """
+    self.ClearBitMask(self.TxControlReg, 0x03)
+
+  def MFRC522_ToCard(self, command, sendData):
+    """
+    Communicate with a card.
+    """
+    backData = []
+    backLen = 0
+    status = self.MI_ERR
+    irqEn = 0x00
+    waitIRq = 0x00
+
+    if command == self.PCD_AUTHENT:
+      irqEn = 0x12
+      waitIRq = 0x10
+    elif command == self.PCD_TRANSCEIVE:
+      irqEn = 0x77
+      waitIRq = 0x30
+
+    self.Write_MFRC522(self.CommIEnReg, irqEn | 0x80)
+    self.ClearBitMask(self.CommIrqReg, 0x80)
+    self.SetBitMask(self.FIFOLevelReg, 0x80)
+    self.Write_MFRC522(self.CommandReg, self.PCD_IDLE)
+
+    for data in sendData:
+      self.Write_MFRC522(self.FIFODataReg, data)
+
+    self.Write_MFRC522(self.CommandReg, command)
+
+    if command == self.PCD_TRANSCEIVE:
+      self.SetBitMask(self.BitFramingReg, 0x80)
+
+    i = 2000
+    while i:
+      n = self.Read_MFRC522(self.CommIrqReg)
+      i -= 1
+      if n & 0x01 or n & waitIRq:
+        break
+
+    self.ClearBitMask(self.BitFramingReg, 0x80)
+
+    if i:
+      if not (self.Read_MFRC522(self.ErrorReg) & 0x1B):
+        status = self.MI_OK
+        if n & irqEn & 0x01:
+          status = self.MI_NOTAGERR
+
+        if command == self.PCD_TRANSCEIVE:
+          n = self.Read_MFRC522(self.FIFOLevelReg)
+          lastBits = self.Read_MFRC522(self.ControlReg) & 0x07
+          backLen = (n - 1) * 8 + lastBits if lastBits else n * 8
+          n = min(n, self.MAX_LEN)
+
+          for _ in range(n):
+            backData.append(self.Read_MFRC522(self.FIFODataReg))
+      else:
+        status = self.MI_ERR
+
+    return status, backData, backLen
+
+  def MFRC522_Request(self, reqMode):
+    """
+    Request a tag.
+    """
+    self.Write_MFRC522(self.BitFramingReg, 0x07)
+    status, backData, backBits = self.MFRC522_ToCard(self.PCD_TRANSCEIVE, [reqMode])
+
+    if status != self.MI_OK or backBits != 0x10:
+      status = self.MI_ERR
+
+    return status, backBits
+
+  def MFRC522_Anticoll(self):
+    """
+    Anti-collision detection.
+    """
+    self.Write_MFRC522(self.BitFramingReg, 0x00)
+    status, backData, backBits = self.MFRC522_ToCard(self.PCD_TRANSCEIVE, [self.PICC_ANTICOLL, 0x20])
+
+    if status == self.MI_OK and len(backData) == 5:
+      serNumCheck = 0
+      for i in range(4):
+        serNumCheck ^= backData[i]
+      if serNumCheck != backData[4]:
+        status = self.MI_ERR
+
+    return status, backData
+
+  def CalculateCRC(self, data):
+    """
+    Calculate CRC.
+    """
+    self.ClearBitMask(self.DivIrqReg, 0x04)
+    self.SetBitMask(self.FIFOLevelReg, 0x80)
+
+    for byte in data:
+      self.Write_MFRC522(self.FIFODataReg, byte)
+
+    self.Write_MFRC522(self.CommandReg, self.PCD_CALCCRC)
+
+    for _ in range(0xFF):
+      n = self.Read_MFRC522(self.DivIrqReg)
+      if n & 0x04:
+        break
+
+    return [self.Read_MFRC522(self.CRCResultRegL), self.Read_MFRC522(self.CRCResultRegM)]
+
+  def MFRC522_SelectTag(self, serNum):
+    """
+    Select a tag.
+    """
+    buf = [self.PICC_SELECTTAG, 0x70] + serNum
+    buf += self.CalculateCRC(buf)
+    status, backData, backLen = self.MFRC522_ToCard(self.PCD_TRANSCEIVE, buf)
+
+    if status == self.MI_OK and backLen == 0x18:
+      logging.info("Size: %s", backData[0])
+      return backData[0]
+    return 0
+
+  def MFRC522_Auth(self, authMode, blockAddr, sectorKey, serNum):
+    """
+    Authenticate a tag.
+    """
+    buff = [authMode, blockAddr] + sectorKey + serNum
+    status, backData, backLen = self.MFRC522_ToCard(self.PCD_AUTHENT, buff)
+
+    if status != self.MI_OK or not (self.Read_MFRC522(self.Status2Reg) & 0x08):
+      logging.error("AUTH ERROR!!")
+
+    return status
+
+  def MFRC522_Read(self, blockAddr):
+    """
+    Read data from a block.
+    """
+    recvData = [self.PICC_READ, blockAddr] + self.CalculateCRC([self.PICC_READ, blockAddr])
+    status, backData, backLen = self.MFRC522_ToCard(self.PCD_TRANSCEIVE, recvData)
+
+    if status == self.MI_OK and len(backData) == 16:
+      logging.info("Sector %s %s", blockAddr, backData)
+    else:
+      logging.error("Error while reading!")
+
+  def MFRC522_Write(self, blockAddr, writeData):
+    """
+    Write data to a block.
+    """
+    buff = [self.PICC_WRITE, blockAddr] + self.CalculateCRC([self.PICC_WRITE, blockAddr])
+    status, backData, backLen = self.MFRC522_ToCard(self.PCD_TRANSCEIVE, buff)
+
+    if status == self.MI_OK and backLen == 4 and (backData[0] & 0x0F) == 0x0A:
+      buf = writeData + self.CalculateCRC(writeData)
+      status, backData, backLen = self.MFRC522_ToCard(self.PCD_TRANSCEIVE, buf)
+      if status == self.MI_OK and backLen == 4 and (backData[0] & 0x0F) == 0x0A:
+        logging.info("Data written")
+      else:
+        logging.error("Error while writing")
+    else:
+      logging.error("Error while writing")
+
+  def MFRC522_Init(self):
+    """
+    Initialize the MFRC522 RFID reader.
+    """
+    GPIO.output(self.NRSTPD, 1)
+    self.MFRC522_Reset()
+
+    self.Write_MFRC522(self.TModeReg, 0x8D)
+    self.Write_MFRC522(self.TPrescalerReg, 0x3E)
+    self.Write_MFRC522(self.TReloadRegL, 30)
+    self.Write_MFRC522(self.TReloadRegH, 0)
+
+    self.Write_MFRC522(self.TxAutoReg, 0x40)
+    self.Write_MFRC522(self.ModeReg, 0x3D)
+    self.AntennaOn()
+
+  def GPIO_CLEAN(self):
+    """
+    Clean up GPIO.
+    """
+    GPIO.cleanup()
+
+def check_rfid():
+  """
+  Check RFID card.
+  """
+  reader = MFRC522()
+  status, card_uid = reader.MFRC522_Anticoll()
+  if status == reader.MI_OK:
+    card_uid = tuple(card_uid)
+    if card_uid in AUTHORIZED_CARDS:
+      card_info = AUTHORIZED_CARDS[card_uid]
+      logging.info("Authorized access: %s, Role: %s", card_info["name"], card_info["role"])
+      return False
+    else:
+      logging.warning("Unauthorized RFID access detected.")
+      return True
+  else:
+    logging.error("Failed to read RFID card.")
+    return True
